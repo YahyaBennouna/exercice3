76aacc4040f3b37302172b5de30002c5ede8550b
benn <benn@DESKTOP-IMTHR3V>
2022-11-27 23:35:46 +0100

    Add project files.

---
+++ ex3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo3
+{
+    internal class Point
+    {
+        private float x, y;
+        Point(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void SetX(float x)
+        {
+            this.x = x;
+        }
+        public float GetX()
+        {
+            return this.x;
+        }
+        public void SetY(float y)
+        {
+            this.y = y;
+        }
+        public float GetY()
+        {
+            return this.y;
+        }
+        public int Distance(Point a, Point b)
+        {
+
+            int distance;
+            a = new Point(x, y);
+            b = new Point(x, y);
+            distance = (int)Math.Sqrt(Math.Pow(a.x - b.x, 2) - Math.Pow(a.y - b.y, 2));
+            return distance;
+
+        }
+        public bool Egalite(Point p)
+        {
+            if (p.x == this.x && p.y == this.y)
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Translation(int k)
+        {
+            SetX(k * this.x);
+            SetY(k * this.y);
+        }
+    }
+}
